8476a4c0d066682b23dff6d82142e9305ca343c6
Yanstime <862415702@qq.com>
2018-09-23 00:39:24 +0800

    taso test

---
+++ taso/global.js
@@ -0,0 +1,18 @@
+{
+	"binhacks": {
+		"meiryo_disable": {
+			"code": "83c414 909090",
+			"title": "Disable the enumeration of Meiryo for compatibility to existing font settings"
+		},
+		"font_dialog_params": {
+			"code": "6a036a006a0068000000006a006a006a006890010000",
+			"title": "Set parameters for alternate dialog font"
+		},
+		"font_dialog_set": {
+			"code": "6a019090",
+			"title": "Use alternate dialog font"
+		}
+	},
+	"font": "FZZZHONGHJW",
+	"thcrap_version_min": "0x20170905"
+}
