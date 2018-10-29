45c9bf599367d0f8eb014c8a26aa5333653b4e39
Yanstime <862415702@qq.com>
2018-10-29 23:29:30 +0800

    test update

--- taso/files.js
+++ taso/files.js
@@ -2,7 +2,7 @@
 	"fzzzh.TTF": null,
 	"fzzzh.ttf": 3517785015,
 	"patch.js": 2115310479,
-	"th155.js": 2661784809,
-	"th155.v1.03.js": 783099900,
-	"th155.v1.10.js": 2982090419
+	"th155.js": null,
+	"th155.v1.03.js": null,
+	"th155.v1.10.js": null
 }
