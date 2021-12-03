a11d617ab0187145eea485341df02fcb0f044d09
Yanstime <862415702@qq.com>
2021-12-03 11:48:21 +0800

    th155: fix 1.10d steam ver. address bug

---
+++ taso/th155.v1.10_steam.js
@@ -0,0 +1,19 @@
+{
+	"binhacks": {
+		"remove_bmpfont_limit": {
+			"code": "9090",
+			"addr": "Rx05cb74"
+		},
+		"remove_sjis_test": {
+			"addr": "Rx0652e6"
+		},
+		"push_char": {
+			"addr": "Rx0653a5"
+		}
+	},
+	"breakpoints": {
+		"bmpfont_fix_parameters": {
+			"addr": "Rx0653a0"
+		}
+	}
+}
