cf2877a06767761619029010a74af63c562ecf64
ROCO2017 <1401548627@qq.com>
2021-12-26 22:19:34 +0800

    优化仓库结构。

--- taso/patch.js
+++ taso/patch.js
@@ -3,13 +3,14 @@
 		"base_tsa"
 	],
 	"fonts": {
-		"msyh.ttc": true,
-		"simhei.ttf": false,
-		"simheig.ttf": true,
-		"syBlod.ttf": true
+		"./font/msyh.ttc": true,
+		"./font/simhei.ttf": false,
+		"./font/simheig.ttf": true,
+		"./font/syBlod.ttf": true
 	},
 	"id": "taso",
 	"servers": [
+		"https://thpatch-cn.coding.net/p/thcrapcn/d/tpZHCNex/git/raw/master/taso/",
 		"https://mirrors.thpatch.net/tpZHCNex/taso/",
 		"https://raw.githubusercontent.com/Yanstory/tpZHCNex/master/taso/"
 	],
