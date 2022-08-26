406c279542bcffbbd415efdde858504a8b80ab96
ROCO2017 <1401548627@qq.com>
2022-08-26 09:35:45 +0800

    Update the repository.

--- taso/patch.js
+++ taso/patch.js
@@ -1,12 +1,14 @@
 {
-	"code": 1000,
-	"data": {
-		"account_type": "0"
+	"dependencies": [
+		"base_tsa"
+	],
+	"fonts": {
+		"msyh.ttc": true,
+		"simhei.ttf": false,
+		"simheig.ttf": true,
+		"syBlod.ttf": true
 	},
 	"id": "taso",
-	"msg": {
-		"user_not_login": "用户未登录"
-	},
 	"servers": [
 		"https://mirrors.thpatch.net/tpZHCNex/taso/",
 		"https://raw.githubusercontent.com/Yanstory/tpZHCNex/master/taso/"
