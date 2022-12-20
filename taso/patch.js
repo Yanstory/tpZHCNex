ab87434207e1fd045fd1fd7ef82ab88c8fb06c18
ROCO2021 <1401548627@qq.com>
2022-12-20 17:41:35 +0800

    添加自动同步至Gitee仓库功能。

--- taso/patch.js
+++ taso/patch.js
@@ -10,6 +10,7 @@
 	},
 	"id": "taso",
 	"servers": [
+		"https://gitee.com/rc17/tpZHCNex/raw/master/taso/",
 		"https://mirrors.thpatch.net/tpZHCNex/taso/",
 		"https://raw.githubusercontent.com/Yanstory/tpZHCNex/master/taso/"
 	],
--- tsa/patch.js
+++ tsa/patch.js
@@ -7,6 +7,7 @@
 	},
 	"id": "tsa",
 	"servers": [
+		"https://gitee.com/rc17/tpZHCNex/raw/master/tsa/",
 		"https://mirrors.thpatch.net/tpZHCNex/tsa/",
 		"https://raw.githubusercontent.com/Yanstory/tpZHCNex/master/tsa/"
 	],
